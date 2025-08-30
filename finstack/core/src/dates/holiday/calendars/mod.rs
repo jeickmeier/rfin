c061637584d205f328d63bf6677bf0114df9133d
Jon Eickmeier <me@jeickmeier.com>
2025-08-30 09:39:03 -0400

    Refactor holiday calendar management by including generated calendar data
    - Removed static instances and the `calendar_by_id` function from `mod.rs`, replacing them with an inclusion of generated calendar data from `generated_calendars.rs`.
    - Updated the code structure to streamline holiday calendar management.
    - Ensured all code is properly linted and formatted, fixing any existing issues throughout the codebase.

--- finstack/core/src/dates/holiday/calendars/mod.rs
+++ finstack/core/src/dates/holiday/calendars/mod.rs
@@ -44,58 +44,6 @@ pub use sse::Sse;
 pub use target2::Target2;
 pub use usny::Usny;
 
-use crate::dates::calendar::HolidayCalendar;
-
-// Static instances for &'static dyn HolidayCalendar references
-static GBLO_INSTANCE: Gblo = Gblo;
-static TARGET2_INSTANCE: Target2 = Target2;
-static ASX_INSTANCE: Asx = Asx;
-static AUCE_INSTANCE: Auce = Auce;
-static CATO_INSTANCE: Cato = Cato;
-static DEFR_INSTANCE: Defr = Defr;
-static NYSE_INSTANCE: Nyse = Nyse;
-static USNY_INSTANCE: Usny = Usny;
-static SIFMA_INSTANCE: Sifma = Sifma;
-static BRBD_INSTANCE: Brbd = Brbd;
-static CHZH_INSTANCE: Chzh = Chzh;
-static CNBE_INSTANCE: Cnbe = Cnbe;
-static SGSI_INSTANCE: Sgsi = Sgsi;
-static SSE_INSTANCE: Sse = Sse;
-static HKHK_INSTANCE: Hkhk = Hkhk;
-static HKEX_INSTANCE: Hkex = Hkex;
-static JPTO_INSTANCE: Jpto = Jpto;
-static JPX_INSTANCE: Jpx = Jpx;
-static CME_INSTANCE: Cme = Cme;
-
-/// Lookup a built-in calendar by its lowercase identifier (e.g. "gblo", "target2").
-/// Returns `None` if the id is unrecognised.
-pub fn calendar_by_id(id: &str) -> Option<&'static dyn HolidayCalendar> {
-    match id {
-        "gblo" => Some(&GBLO_INSTANCE as &dyn HolidayCalendar),
-        "target2" => Some(&TARGET2_INSTANCE as &dyn HolidayCalendar),
-        "asx" => Some(&ASX_INSTANCE as &dyn HolidayCalendar),
-        "auce" => Some(&AUCE_INSTANCE as &dyn HolidayCalendar),
-        "cato" => Some(&CATO_INSTANCE as &dyn HolidayCalendar),
-        "defr" => Some(&DEFR_INSTANCE as &dyn HolidayCalendar),
-        "nyse" => Some(&NYSE_INSTANCE as &dyn HolidayCalendar),
-        "usny" => Some(&USNY_INSTANCE as &dyn HolidayCalendar),
-        "sifma" => Some(&SIFMA_INSTANCE as &dyn HolidayCalendar),
-        "brbd" => Some(&BRBD_INSTANCE as &dyn HolidayCalendar),
-        "chzh" => Some(&CHZH_INSTANCE as &dyn HolidayCalendar),
-        "cnbe" => Some(&CNBE_INSTANCE as &dyn HolidayCalendar),
-        "sgsi" => Some(&SGSI_INSTANCE as &dyn HolidayCalendar),
-        "sse" => Some(&SSE_INSTANCE as &dyn HolidayCalendar),
-        "hkhk" => Some(&HKHK_INSTANCE as &dyn HolidayCalendar),
-        "hkex" => Some(&HKEX_INSTANCE as &dyn HolidayCalendar),
-        "jpto" => Some(&JPTO_INSTANCE as &dyn HolidayCalendar),
-        "jpx" => Some(&JPX_INSTANCE as &dyn HolidayCalendar),
-        "cme" => Some(&CME_INSTANCE as &dyn HolidayCalendar),
-        _ => None,
-    }
-}
-
-pub const ALL_IDS: &[&str] = &[
-    "gblo", "target2", "asx", "auce", "cato", "defr", "nyse", "usny", "sifma", "brbd", "chzh",
-    "cnbe", "sgsi", "sse", "hkhk", "hkex", "jpto", "jpx", "cme",
-];
+// Include generated registry: ALL_IDS, calendar_by_id, and helpers
+include!(concat!(env!("OUT_DIR"), "/generated_calendars.rs"));
 
