6c2943e0c22f6d5506db0ae7b4a43a7fc37f9ad3
jeickmeier <me@jeickmeier.com>
2026-03-22 08:58:32 -0400

    feat: populate analytics prelude with common re-exports

--- finstack/statements-analytics/src/prelude.rs
+++ finstack/statements-analytics/src/prelude.rs
@@ -1,1 +1,16 @@
-//! Re-exports of the most common analytics types.
+//! Commonly used analytics types.
+//!
+//! Import this module to get quick access to the most common analytics types:
+//!
+//! ```rust,ignore
+//! use finstack_statements_analytics::prelude::*;
+//! ```
+
+pub use crate::analysis::{
+    BridgeChart, BridgeStep, CorporateAnalysis, CorporateAnalysisBuilder,
+    CreditInstrumentAnalysis, MonteCarloConfig, MonteCarloResults,
+    ScenarioDefinition, ScenarioDiff, ScenarioResults, ScenarioSet,
+    VarianceAnalyzer, VarianceConfig, VarianceReport, VarianceRow,
+};
+pub use crate::extensions::{CorkscrewExtension, CreditScorecardExtension};
+pub use crate::templates::{RealEstateExtension, TemplatesExtension, VintageExtension};
